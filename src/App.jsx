1a833e7aa0a0064a891261acd3daadd3c463ef47
IvMysl <IvMysl_2009@gmail.com>
2024-11-02 11:42:46 +0200

    done slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Features from "./components/Features";
 import Feedbacks from "./components/Feedbacks";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Slider from "./components/Slider";
 import SystemRequirements from "./components/SystemRequirements";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Hero/>
       {/* <Component/> */}
+      <Slider/>
       <Features/>
       <SystemRequirements/>
       <Feedbacks/>
